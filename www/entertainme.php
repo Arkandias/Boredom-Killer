a44e517a4dc1477e62d936ba0e054fce71c43ceb
Kagaminara Shikatsu <misterarkandias@gmail.com>
2017-03-25 22:40:28 +0100

    Reindented to fit weird and boring text editors

--- www/entertainme.php
+++ www/entertainme.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Your lazy game selector</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+  <title>Your lazy game selector</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-<?php
-
-class SteamApi {
-    $url = 'http://...';
-    public function AppDetails($gameID) {
-    }
-}
-
-$api = SteamApi();
-?>	
+<div>
+  <?php
+  class SteamApi {
+      $url = 'http://...';
+      public function AppDetails($gameID) {
+      }
+  }
+  $api = SteamApi();
+  ?>  
+</div>
 </body>
 </html>
 
