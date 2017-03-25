4138ba95a43c8799fe5d834f46476bcaca0f0338
obayemi <obayemi@ikjpz.com>
2017-03-25 23:47:06 +0100

    better players

--- www/entertainme.php
+++ www/entertainme.php
@@ -15,6 +15,10 @@ class Player {
         $this->name = $name;
         $this->id = $id;
     }
+
+    public getOwnedGames() {
+        $this->game_list = $api->getOwnedGames($this->id);
+    }
   }
 ?>
 <!DOCTYPE html>
@@ -30,6 +34,8 @@ class Player {
 //$player2 = $_REQUEST['player2'];
 $player1 = new Player('76561197997640408');
 $player2 = new Player('76561197994769476');
+
+$player2
 
 echo $api->AppDetails('404');
     ?>
