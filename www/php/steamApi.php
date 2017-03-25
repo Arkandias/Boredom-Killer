f32972b420d8a59f047e193c7b45e2cb631fb491
Kagaminara Shikatsu <misterarkandias@gmail.com>
2017-03-26 00:14:36 +0100

    Added GetIdFromUrl method to steamApi

--- www/php/steamApi.php
+++ www/php/steamApi.php
@@ -7,4 +7,13 @@ class SteamApi {
   public function GetOwnedGames($userID) {
     return json_decode(file_get_contents("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=E44E540CAB2E08BB0B4A471117B8DA24&steamid={$userID}&format=json"), true)['response']['games'];
   }
+
+  public function GetIdFromUrl($url) {
+    $result = json_decode(file_get_contents("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=E44E540CAB2E08BB0B4A471117B8DA24&vanityurl={$url}"), true)['response'];
+    if ($result['success'] and isset($result['steamid'])) {
+      return $result['steamid'];
+    }
+    return "Custom URL not found";
+  }
+
 }
