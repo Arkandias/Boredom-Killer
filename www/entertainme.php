538998350e140f1e4b553465a5184ff74306bb11
Kagaminara Shikatsu <misterarkandias@gmail.com>
2017-03-26 00:53:28 +0100

    Don't remember what I exactly added in this commit. Lol.

--- www/entertainme.php
+++ www/entertainme.php
@@ -3,15 +3,20 @@ require_once 'php/steamApi.php';
 $api = new SteamApi();
 
 class Player {
-    public $name;
+    // public $name;
     public $id;
     public $game_list;
 
-    function __construct($id, $name = false) {
+    function __construct($id = false, $name = false) {
+      global $api;
       $this->id = $id;
       if ($name) {
-        $this->name = $name;
+        if (($result = $api->GetIdFromUrl($name))) {
+          $this->id = $result;
+        };
       }
+      // Retrieving user's display name
+      // $this->name = $api->GetPlayerSummaries($this->id)['personaname'];
     }
 
     public function getOwnedGames() {
@@ -31,13 +36,14 @@ class Player {
     <?php
       //$player1 = $_REQUEST['player1'];
       //$player2 = $_REQUEST['player2'];
-      $player1 = new Player('76561197997640408');
-      $player2 = new Player('76561197994769476');
+      $player1 = new Player($id = false, $name = 'arkandiasmaniac');
+      $player2 = new Player('76561197994769476', 'obayemi');
 
-      $player1->getOwnedGames();
-      foreach ($player1->game_list as $key) {
-        echo "<p>{$key['appid']}</p>";
-      }
+      // $player1->getOwnedGames();
+      // foreach ($player1->game_list as $key) {
+      //   echo "<p>{$key['appid']}</p>";
+      // }
+      // echo $player1->name;
     ?>
   </div>
 </body>
--- www/php/steamApi.php
+++ www/php/steamApi.php
@@ -8,12 +8,16 @@ class SteamApi {
     return json_decode(file_get_contents("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=E44E540CAB2E08BB0B4A471117B8DA24&steamid={$userID}&format=json"), true)['response']['games'];
   }
 
+  public function GetPlayerSummaries($userID) {
+    return json_decode(file_get_contents("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=E44E540CAB2E08BB0B4A471117B8DA24&steamids={$userID}&format=json"), true)['response']['players'][0];
+  }
+
   public function GetIdFromUrl($url) {
     $result = json_decode(file_get_contents("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=E44E540CAB2E08BB0B4A471117B8DA24&vanityurl={$url}"), true)['response'];
     if ($result['success'] and isset($result['steamid'])) {
       return $result['steamid'];
     }
-    return "Custom URL not found";
+    return 0;
   }
 
 }
