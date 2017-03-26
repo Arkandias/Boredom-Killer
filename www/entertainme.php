f4b50f2b525b5dd56e7bda697465f79c7e02b253
obayemi <obayemi@ikjpz.com>
2017-03-26 01:17:43 +0100

    get filter get only common games

--- www/entertainme.php
+++ www/entertainme.php
@@ -16,9 +16,16 @@ class Player {
 
     public function getOwnedGames() {
       global $api;
-      $this->game_list = $api->GetOwnedGames($this->id);
+      return $this->game_list = array_map(
+          function($x) {
+              return $x['appid'];
+          },
+          $api->GetOwnedGames($this->id));
     }
   }
+
+$player1 = new Player('76561197997640408');
+$player2 = new Player('76561197994769476');
 ?>
 <!DOCTYPE html>
 <html>
@@ -28,17 +35,26 @@ class Player {
 </head>
 <body>
   <div>
-    <?php
-      //$player1 = $_REQUEST['player1'];
-      //$player2 = $_REQUEST['player2'];
-      $player1 = new Player('76561197997640408');
-      $player2 = new Player('76561197994769476');
-
-      $player1->getOwnedGames();
-      foreach ($player1->game_list as $key) {
-        echo "<p>{$key['appid']}</p>";
-      }
-    ?>
+    <h3>Available games:<h3>
+    <ul>
+      <?php
+        $player1->getOwnedGames();
+        $player2->getOwnedGames();
+        $commonGames = array_intersect(
+            $player1->game_list,
+            $player2->game_list);
+        $detailed = array_map(
+            function($gameID) {
+                global $api;
+                return $api->AppDetails($gameID);
+            },
+            $commonGames
+        );
+        foreach ($detailed as $game) {
+            echo "<li>{$game['name']}</li>";
+        }
+      ?>
+    </ul>
   </div>
 </body>
 </html>
